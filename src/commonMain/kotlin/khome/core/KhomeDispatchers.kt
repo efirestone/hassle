c6e9046fc3f5cb8bfa1e04da410895c1abb79ae7
efirestone <efirestone@users.noreply.github.com>
2021-06-29 23:05:11 -1000

    Remove unused coroutine dispatchers and update Command dispatcher
    This now uses the default dispatcher since that one's multiplatform compatible.

--- src/commonMain/kotlin/khome/core/KhomeDispatchers.kt
+++ src/commonMain/kotlin/khome/core/KhomeDispatchers.kt
@@ -1,14 +1,7 @@
 package khome.core
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
 
-@OptIn(ExperimentalCoroutinesApi::class)
 object KhomeDispatchers {
-    internal val CommandDispatcher = newSingleThreadContext("command-dispatcher")
-    val Unconfined = Dispatchers.Unconfined
-    val SingleThread = newSingleThreadContext("single-thread-dispatcher")
-    val CPUBound = Dispatchers.Default
-    val IOBound = Dispatchers.IO
+    internal val CommandDispatcher = Dispatchers.Default
 }
