7ce86326629bf80ddf59616e1f741fc54c39040f
efirestone <efirestone@users.noreply.github.com>
2021-06-29 18:05:21 -1000

    chore: Set up ConcurrentHashMap for multiplatform replacement

--- src/commonMain/kotlin/khome/core/boot/servicestore/ServiceStore.kt
+++ src/commonMain/kotlin/khome/core/boot/servicestore/ServiceStore.kt
@@ -3,7 +3,7 @@ package khome.core.boot.servicestore
 import java.util.concurrent.ConcurrentHashMap
 
 interface ServiceStoreInterface {
-    val list: ConcurrentHashMap<String, List<String>>
+    val list: MutableMap<String, List<String>>
 
     operator fun set(domain: String, services: List<String>)
     operator fun get(domain: String) = list[domain]
@@ -13,7 +13,7 @@ interface ServiceStoreInterface {
 
 internal class ServiceStore :
     Iterable<MutableMap.MutableEntry<String, List<String>>>, ServiceStoreInterface {
-    override val list = ConcurrentHashMap<String, List<String>>()
+    override val list = createMapForServiceStore<String, List<String>>(10)
     override operator fun iterator() = list.iterator()
     override operator fun set(domain: String, services: List<String>) {
         list[domain] = services
@@ -22,3 +22,10 @@ internal class ServiceStore :
     override operator fun contains(domain: String) = list.containsKey(domain)
     override fun clear() = list.clear()
 }
+
+/**
+ * Creates a ConcurrentHashMap. This method should be made multiplatform compatible.
+ */
+internal fun <K, V> createMapForServiceStore(initialCapacity: Int): MutableMap<K, V> =
+    ConcurrentHashMap(initialCapacity)
+
