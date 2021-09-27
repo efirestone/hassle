51bb7cc84fe1a5fd9ca8b785af8fa7bd8914bde9
efirestone <efirestone@users.noreply.github.com>
2021-09-26 19:17:37 -0500

    chore: Upgrade to Kotlin 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.31"
     kotlin("plugin.serialization") version "1.5.0"
     id("org.jetbrains.dokka") version "1.4.32"
     `maven-publish`
