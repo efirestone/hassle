6e05adfa82441cb478999087dc5a79620c9deb13
efirestone <efirestone@users.noreply.github.com>
2021-10-17 00:29:38 -0500

    fix: Remove duplicate logging
    This same log message is logged in HassApi and was leading to duplicates

--- src/commonMain/kotlin/khome/WebSocketSession.kt
+++ src/commonMain/kotlin/khome/WebSocketSession.kt
@@ -14,12 +14,9 @@ internal class WebSocketSession(
 ) : ClientWebSocketSession by delegate {
 
     private val logger = Kermit()
-    suspend fun callWebSocketApi(message: String) =
-        send(message).also { logger.d { "Called hass api with message: $message" } }
+    suspend fun callWebSocketApi(message: String) = send(message)
 
-    suspend inline fun <reified M : Any> callWebSocketApi(message: M) =
-        send(objectMapper.toJson(message))
-            .also { logger.d { "Called hass api with message: ${objectMapper.toJson(message)}" } }
+    suspend inline fun <reified M : Any> callWebSocketApi(message: M) = send(objectMapper.toJson(message))
 
     suspend inline fun <reified M : Any> consumeSingleMessage(): M = incoming.receive().asObject()
     inline fun <reified M : Any> Frame.asObject(): M = (this as Frame.Text).toObject()
